168117eb663acc83ff51a3d12a43a6c9aeb21d3c
Del Piero <delpiero22.flores@gmail.com>
2024-10-05 19:10:48 -0500

    hour 08:22

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,14 +1,10 @@
 import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
-
+import List from "./tutorial/05-leverage-javascript/starter/List";
 function App() {
   return (
     <div className="container">
-      <h2>Advanced React</h2>
-      <h3 style={{ color: "red" }}>Example</h3>
-      <Final></Final>
-      <h3 style={{ color: "red" }}>Implementation</h3>
-      <Starter />
+      <List />
     </div>
   );
 }
